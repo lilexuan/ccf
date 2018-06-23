fcd9f5e545720245ed9cc6889536692d25bc269d
unknown <834524281@qq.com>
2018-06-23 14:45:43 +0800

    匹配函数有问题...待修改

--- 201803-3.cpp
+++ 201803-3.cpp
@@ -8,7 +8,8 @@ using namespace std;
 const int MAX = 200;
 pair<string, string> p[MAX]; // 规则串 
 int n, m;
-vector<string> regStr[MAX]; // 规则串的信息 
+vector<string> regStr[MAX]; // 规则串的信息
+vector<string> matPara; // 匹配串的参数 
 
 // 将/转化为空格的函数
 void replaceSpace(string &ss) {
@@ -40,6 +41,23 @@ bool isNumber(string str) {
 	return true;
 } 
 
+// 判断一个字符串是否合法字符串
+bool isStr(string str) {
+	int len = str.size();
+	for (int i = 0; i < len; i++) {
+		if ('a' <= str[i] && str[i] <= 'z') {
+			continue;
+		} else if ('A' <= str[i] && str[i] <= 'Z') {
+			continue;
+		} else if (str[i] == '-' || str[i] == '.' || str[i] == '_') {
+			continue;
+		} else {
+			return false;
+		}
+	}
+	return true;
+} 
+
 // 数字类型的字符串去掉前面的零
 string strNoZero(string s) {
 	int len = s.size();
@@ -54,10 +72,10 @@ string strNoZero(string s) {
 } 
 
 // 匹配函数 
-void match(string str) {
-	vector<string> matStr; // 匹配串的信息 
-	vector<string> matPara; // 匹配串的参数 
+int match(string str) {
+	vector<string> matStr; // 匹配串的信息  
 	bool matFlag = true;
+	int result = -1; // 返回的参数, 表明匹配了第几条规则 
 	
 	// 将匹配串的信息录入到matStr里面 
 	stringstream in(str);
@@ -75,29 +93,77 @@ void match(string str) {
 			break;
 		}
 		// 对规则串里的每一条规则进行对比
-		for (int j = 0; j < regStr || j < matStr; j++) {
+		for (int j = 0; j < regSize || j < matSize; j++) {
 			if (regStr[i][j] == "<int>") {
-				if (isNumber(matStr[i]) == false) {
+				if (isNumber(matStr[j]) == false) {
 					matFlag = false;
 					break;
 				} else {
-					string number = strNoZero(matStr[i]);
+					string number = strNoZero(matStr[j]);
 					matPara.push_back(number);
 				}
 			} else if (regStr[i][j] == "<str>") {
-				
+				if (isStr(matStr[j]) == false) {
+					matFlag = false;
+					break;
+				} else {
+					matPara.push_back(matStr[j]);
+				}
 			} else if (regStr[i][j] == "<path>") {
-				
+				for (int k = j; k < matSize; k++) {
+					if (isStr(matStr[k]) == true) {
+						matPara.push_back(matStr[k]);
+					} else {
+						matFlag = false;
+						break;
+					}
+				}
+				if (matFlag == false) {
+					break;
+				}
 			} else {
-				
+				if (regStr[i][j] != matStr[j]) {
+					matFlag = false;
+				}
 			}
-		} 
-		if (matFlag == false) {
-			break;
 		}
-	} 
+		if (matFlag == true) { // 匹配串符合当前规则串, 则不用再检测了 
+			result = i;
+			break;
+		} else {
+			continue;
+		}
+	}
+	return result;
 }
 
+// 打印函数
+void display(int result, bool divFlag) {
+	cout << p[result].second;
+	int size = regStr[result].size();
+	if (regStr[result][size - 1] != "<path>") {
+		for (int i = 0; i < matPara.size(); i++) {
+			cout << " " << matPara[i];
+		}
+		cout << endl;
+	} else {
+		int cnt = 0;
+		for (int i = 0; i < size; i++) {
+			if (regStr[result][i] == "<str>" || regStr[result][i] == "<int>") {
+				cnt++;
+			}
+		}
+		for (int i = 0; i < cnt; i++) {
+			cout << " " << matPara[i];
+		}
+		for (int i = cnt; i < matPara.size(); i++) {
+			cout << "/" << matPara[i];
+		}
+		if (divFlag) {
+			cout << "/" << endl;
+		}
+	}
+} 
 int main() {
 	// 输入匹配规则和匹配字符串 
 	cin >> n >> m;
@@ -108,16 +174,6 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		cin >> s[i];
 	}
-	
-	// 测试输入
-	/* 
-	for (int i = 0; i < n; i++) {
-		cout << p[i].first << " " << p[i].second << endl;
-	}	 
-	for (int i = 0; i < m; i++) {
-		cout << s[i] << endl;
-	}
-	*/
 	
 	// 对规则串进行预处理,将/替换为空格
 	for (int i = 0; i < n; i++) {
@@ -130,23 +186,25 @@ int main() {
 		restoreReg(p[i].first, i);
 	} 
 	
-	// 测试regStr
-	/*
-	for (int i = 0; i < n; i++) {
-		int size = regStr[i].size();
-		for (int j = 0; j < size; j++) {
-			if (j) {
-				cout << " ";
-			}
-			cout << regStr[i][j];
-		}
-		cout << endl;
-	} 
-	*/
-	
 	// 对m个字符串开始匹配 
 	for (int i = 0; i < m; i++) {
-		match(s[i]);
+		int result = match(s[i]);
+		
+		// 标记当前匹配串最后一个字符是不是/ 
+		bool divFlag;
+		int sSize = s[i].size();
+		if (s[i][sSize - 1] == '/') {
+			divFlag = true;
+		} else {
+			divFlag = false;
+		}
+		
+		if (result == -1) {
+			cout << 404 << endl;
+		} else {
+			display(result, divFlag);
+		}
+		matPara.clear(); // 清空匹配串参数, 以便下一个使用 
 	}
 	return 0;
 }
